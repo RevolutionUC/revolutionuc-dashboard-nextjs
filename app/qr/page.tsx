d40513d319094e2997d3065d376a87543f9e7624
Amaan Bilwar <bilwarad@mail.uc.edu>
2026-01-08 23:53:33 -0500

    hotfix: fix infinite loop error

--- app/qr/page.tsx
+++ app/qr/page.tsx
@@ -1,19 +1,17 @@
-import { Scanner } from '@yudiel/react-qr-scanner';
+"use client";
+import { Scanner } from "@yudiel/react-qr-scanner";
 
 function BasicExample() {
   const handleScan = (detectedCodes) => {
-    console.log('Detected codes:', detectedCodes);
+    console.log("Detected codes:", detectedCodes);
     // detectedCodes is an array of IDetectedBarcode objects
-    detectedCodes.forEach(code => {
+    detectedCodes.forEach((code) => {
       console.log(`Format: ${code.format}, Value: ${code.rawValue}`);
     });
   };
 
   return (
-    <Scanner
-      onScan={handleScan}
-      onError={(error) => console.error(error)}
-    />
+    <Scanner onScan={handleScan} onError={(error) => console.error(error)} />
   );
 }
 
